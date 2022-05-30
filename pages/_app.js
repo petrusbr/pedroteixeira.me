d934bace6033a80bdb19838134a4c44e3e4f5531
Pedro Teixeira <pedrao@gmail.com>
2022-05-29 23:17:27 -0300

    1 commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
+      <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                  var _bsq = _bsq || [];
+                  _bsq.push(['init', '2277']);
+                  _bsq.push(['track', 'PageView']);
+
+                  (function(w, d, s, id){
+                      var js, fjs = d.getElementsByTagName(s)[0];
+                      if (d.getElementById(id)) { return; }
+                      js = d.createElement(s); js.id = id; 
+                      js.src = 'https://cdn.birdsend.co/assets/static/js/pixel/main.js';
+                      fjs.parentNode.insertBefore(js, fjs);
+                  }(window, document, 'script', 'birdsend-pixel-js'));
+
+            `,
+            }}
+          />
       <Head>
         <link
           rel="alternate"
--- pages/_document.js
+++ pages/_document.js
@@ -8,14 +8,14 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'Pedro Teixeira',
+      description: 'Venda mais usando o Email e gaste menos com anúncios',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
 
     return (
-      <Html lang="en">
+      <Html lang="pt-BR">
         <Head>
           <meta name="robots" content="follow, index" />
           <meta name="description" content={meta.description} />
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
+          <meta name="twitter:site" content="@lambdapetrus" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
           <meta name="twitter:image" content={meta.image} />
